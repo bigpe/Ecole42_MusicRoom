1f34b77fc051d070d8051e92daa6ae94ab85ccbe
Aleksandr Sokolov <52139992+bigpe@users.noreply.github.com>
2022-06-19 02:02:51 +1000

    Feature: Create super user

---
+++ backend/create_superuser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
+
+import django
+
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+try:
+    User.objects.create_superuser('test', 'test@example.com', 'test')
+except Exception:
+    pass
