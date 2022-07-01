cd2d508aec4da5f699beef9064780766ef88de0f
Aleksandr Sokolov <52139992+bigpe@users.noreply.github.com>
2022-07-01 23:04:54 +0300

    Fix: Access token life time (5m to 24h)

--- backend/django_app/settings.py
+++ backend/django_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -168,4 +168,5 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60 * 24),
 }
